696a19de36b4977ae8367a5580297228f26d451d
Lakshya Saharan <lakshyasaharan@gmail.com>
2026-04-20 15:56:57 -0400

    frontend: create dashboard card and add shadcn

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,33 +1,32 @@
 "use client";
 
 import clsx from "clsx";
-import { JSX } from "react";
+import { Button } from "@/components/ui/button"
+import { ScrollArea } from "@/components/ui/scroll-area"
 
 export default function Home() {  
 
-  function rows(): JSX.Element[] {
-    const rowArray = [];
-    for (let i = 0; i < 5; i++) {
-      rowArray.push(
-        <div key={i} className="w-full h-10 border-2 border-purple-300"></div>
-      );
-    }
-    return rowArray;
-  }
-
   return (
     <div className={clsx(
-      "h-screen",
-      "border-5",
-      "border-red-500",
-      "flex flex-col",
-      "gap-5"
+      "h-screen",            
+      "flex",
+      "justify-center",
+      "items-center"
     )}>
-      <div className="w-full h-20 sm:h-40 border-5 border-blue-500 flex justify-center items-center">
-        <button>Upload</button>
-      </div>
-      <div className="w-full h-full border-5 border-blue-500 flex flex-col gap-6">
-        {rows()}
+      <div className="w-60 h-100 sm:w-150 sm:h-120 xl:w-200 xl:h-180 rounded-xl flex flex-col shadow-2xl p-2">        
+        <div className="h-16 bg-gray-50 flex items-center justify-between rounded-t-xl px-4">
+          <h1 className="text-lg sm:text-2xl font-semibold tracking-tight">
+            Space<span className="text-gray-400">Cloud</span>
+          </h1>
+          <Button variant="outline" size="lg">Upload</Button>
+        </div>
+        <ScrollArea className="flex-1 min-h-0">
+          <div className="h-80 bg-blue-100 rounded-b-xl"></div>
+          <div className="h-80 bg-green-100 rounded-b-xl"></div>
+          <div className="h-80 bg-blue-100 rounded-b-xl"></div>
+          <div className="h-80 bg-green-100 rounded-b-xl"></div>
+        </ScrollArea>
+      
       </div>
     </div>
   );
